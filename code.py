430f69b4990e25941fb729253b3f89e9d54b3cc0
Gagan <81000542+GaganSinghGit@users.noreply.github.com>
2021-09-25 01:06:52 +1000

    a

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Path
+from pydantic import BaseModel 
 
 app = FastAPI()
 
@@ -7,6 +7,24 @@ app = FastAPI()
 def home():
     return {"Data":"Testing"}
 
-@app.get("/abput")
+@app.get("/about")
 def about():
     return {"Data":"about"}
+
+inventory = {
+        1:{
+            "name":"Milk",
+            "price":2.99,
+            "brand":"Regular"
+        }
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None,description="asdfasdfdasf")):
+    return inventory[item_id]
+
+
+@app.post("/crerate-item")
+def create_item():
+    
+    return 
